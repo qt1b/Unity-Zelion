a8280391c0e512baee28f581bec1336778cc70c7
qt1b <145593439+qt1b@users.noreply.github.com>
2024-02-09 18:07:48 +0100

    merci ad, le mouvement + dash marchent a peu près

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -8,6 +8,14 @@ public class MovePlayer : MonoBehaviour
     private Rigidbody2D myRigidBody;
     private Vector3 change;
 
+    //variables pour le dash
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashPower = 5f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +23,53 @@ public class MovePlayer : MonoBehaviour
     }
 
     // Update is called once per frame
+    // 
     void Update()
     {
+        if(isDashing){return;}
+    
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        change.Normalize();
+
         // Debug.Log(change);
         if (change != Vector3.zero) {
             MoveCharacter();
         }
+
+        if(Input.GetKey(KeyCode.LeftShift) && canDash==true)
+        {
+            StartCoroutine(Dash());
+        }
+        Debug.Log(change);
+
+    }
+    void MoveCharacter()
+    {
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            myRigidBody.MovePosition(transform.position + change * (speed*2) * Time.deltaTime);
+        }
+        else
+        {
+            myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+        }
+
     }
 
-    void MoveCharacter() {
-        myRigidBody.MovePosition(
-            transform.position + change * speed * Time.deltaTime
-        );
+    // on ne peut pas dash vers le haut, probleme si on appuie plusieurs fois rapidements pour dash
+    private IEnumerator Dash(){
+        canDash = false;
+        isDashing = true;
+        if(change.y != 0 || change.x != 0)
+        {
+            myRigidBody.MovePosition(transform.position + change * dashPower);
+        }
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
+
 }
