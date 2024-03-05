c29466ad2ae578e3eda84e6e4acec0b0a695e400
qt1b <145593439+qt1b@users.noreply.github.com>
2024-03-05 16:53:43 +0100

    got a breaking object working, now i have to make these triggers active only when required

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Smash() {
+        anim.SetBool("cut",true);
+        StartCoroutine(delCollision());
+    }
+
+    IEnumerator delCollision() {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
