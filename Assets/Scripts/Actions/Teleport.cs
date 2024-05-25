a1fdf63b8df97e53d328fc4dc9fc8925078890ed
qt1b <145593439+qt1b@users.noreply.github.com>
2024-05-25 18:58:11 +0200

    teleport quickfix for the camera

--- Assets/Scripts/Actions/Teleport.cs
+++ Assets/Scripts/Actions/Teleport.cs
@@ -8,22 +8,23 @@ namespace Actions {
         //[FormerlySerializedAs("X")] public float x;
         //[FormerlySerializedAs("Y")] public float y;
         public string hitTag;
-        public GameObject player;
+        private GameObject _player;
         public Vector3 goal;
         private Camera _camera;
         void Start() {
             _camera = Camera.main;
+            _player = Player.Player.LocalPlayerInstance;
         }
 
         public void Activate() {
-            player.transform.position = goal;
-            _camera.transform.position = goal;
+            _player.transform.position = goal;
+            _camera.transform.position = new Vector3(goal.x,goal.y,-1);
         }
 
         void OnTriggerEnter2D(Collider2D other) {
             if (other.CompareTag(hitTag)) {
                 other.transform.position = goal;
-                _camera.transform.position = goal;
+                _camera.transform.position = new Vector3(goal.x,goal.y,-1);
             }
         }
     }
